287224be31f452db946975c02aa37edc75c2826a
krashoka <kumarashoka7677@gmail.com>
2022-05-21 10:28:23 +0530

    navbar completed

---
+++ src/components/Links.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Links = ()=>{
+    return(
+        <div>
+            <span>Services</span>&nbsp;&nbsp;&nbsp;&nbsp;
+            <span>Projects</span>&nbsp;&nbsp;&nbsp;&nbsp;
+            <span>About</span>
+        </div>
+    );
+};
+
+export default Links;
